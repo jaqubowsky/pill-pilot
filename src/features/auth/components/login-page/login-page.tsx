c1f7854469c779226b3fc62891368c54efdfadd2
Jakub Nalewajk <jaqubowskyy@gmail.com>
2026-03-26 16:16:17 +0100

    fix: login page look on desktop

--- src/features/auth/components/login-page/login-page.tsx
+++ src/features/auth/components/login-page/login-page.tsx
@@ -1,12 +1,12 @@
-import { LoginButton } from "./login-button";
-import { LoginHeader } from "./login-header";
+import { LoginButton } from './login-button'
+import { LoginHeader } from './login-header'
 
 export function LoginPage() {
-	return (
-		<div className="flex min-h-screen flex-col items-center justify-center bg-surface px-md">
-			<LoginHeader />
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center bg-surface px-md max-w-lg mx-auto gap-md">
+      <LoginHeader />
 
-			<LoginButton />
-		</div>
-	);
+      <LoginButton />
+    </div>
+  )
 }
